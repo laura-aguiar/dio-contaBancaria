93838b4fdeb4fad22f2d38dd3313285e27253ce7
laura-aguiar <beatrizlaura863@gmail.com>
2024-09-12 17:09:02 -0400

    atualização

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO:CONHECER E IMPORTAR A CLASSE SCANNER
+        //EXIBIR AS MSGS P O NOSSO USUARIO
+        //OBTER PELA SCANNER OS VALORES DIGITADOS
+        //EXIBIR A MSG CONTA CRIADA
+
+        Scanner scanner = new Scanner(System.in);
+        //criando o objeto scanner p receber dados
+
+        System.out.println("Por favor, digite o número da sua conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.println(" Por favor, digite o nome do cliente: ");
+        scanner.nextLine();
+        String nomeCliente = scanner.nextLine();
+
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+
+
+
+
     }
 }
